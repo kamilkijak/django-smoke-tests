a4e14854339cc8f0677e32390d9a974266007c01
Kamil Kijak <kamilkijak@gmail.com>
2017-10-15 13:39:35 +0200

    Refactor, split into functions

--- django_smoke_tests/management/commands/smoke_tests.py
+++ django_smoke_tests/management/commands/smoke_tests.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.core.management import BaseCommand, call_command
 
 try:
@@ -6,22 +9,6 @@ except ImportError:
     from django.core.urlresolvers import get_resolver
 
 from ...tests import SmokeTests
-
-
-def _test_generator(url, method, detail_url=False):
-    def test(self):
-        if method == 'GET':
-            response = self.client.get(url)
-        elif method == 'POST':
-            response = self.client.post(url, {})
-        elif method == 'DELETE':
-            response = self.client.delete(url)
-
-        allowed_status_codes = [200, 201, 301, 302, 304, 405]
-        if detail_url:
-            allowed_status_codes.append(404)
-        self.assertIn(response.status_code, allowed_status_codes)
-    return test
 
 
 class Command(BaseCommand):
@@ -34,18 +21,45 @@ class Command(BaseCommand):
         all_endpoints = get_resolver(None).reverse_dict
 
         for endpoint, endpoint_params in all_endpoints.items():
-            if isinstance(endpoint, str):
-                [(url_as_str, url_params)], url_pattern, _ = endpoint_params
-
-                mocked_params = {param: 'random' for param in url_params}
-
-                ready_url = url_as_str % mocked_params
-                ready_url = ready_url if ready_url.startswith('/') else '/{}'.format(ready_url)
-                self.create_tests_for_endpoint(ready_url, endpoint, detail_url=bool(url_params))
+            self.create_tests_for_endpoint(endpoint, endpoint_params)
 
         call_command('test', 'django_smoke_tests')
 
-    def create_tests_for_endpoint(self, url, endpoint, detail_url=False):
+    @staticmethod
+    def _test_generator(url, method, detail_url=False):
+        def test(self):
+            if method == 'GET':
+                response = self.client.get(url)
+            elif method == 'POST':
+                response = self.client.post(url, {})
+            elif method == 'DELETE':
+                response = self.client.delete(url)
+
+            allowed_status_codes = [200, 201, 301, 302, 304, 405]
+            if detail_url:
+                allowed_status_codes.append(404)
+            self.assertIn(response.status_code, allowed_status_codes)
+
+        return test
+
+    def create_tests_for_endpoint(self, endpoint, endpoint_params):
+        if isinstance(endpoint, str):
+            [(url_as_str, url_params)], url_pattern, _ = endpoint_params
+
+            fake_params = {param: self.create_random_string() for param in url_params}
+            url = self.create_url(url_as_str, fake_params)
+            self.create_tests_for_http_methods(url, endpoint, detail_url=bool(url_params))
+
+    @staticmethod
+    def create_random_string(length=5):
+        return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+
+    @staticmethod
+    def create_url(url_as_str, parameters):
+        url = url_as_str % parameters
+        return url if url.startswith('/') else '/{}'.format(url)
+
+    def create_tests_for_http_methods(self, url, endpoint, detail_url=False):
         for method in self.METHODS_TO_TEST:
-            test = _test_generator(url, method, detail_url)
+            test = self._test_generator(url, method, detail_url)
             setattr(SmokeTests, 'test_smoke_{}_{}'.format(method, endpoint), test)
